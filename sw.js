37be47ada1c0cf5200def302e454fd2588d64f9e
prashant.mohite <prashant.mohite@citnowgroup-partners.com>
2026-02-09 11:24:35 +0530

    fix

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "fantasy-cricket-v1";
+const URLS_TO_CACHE = [
+  "/fantasy-cricket/",
+  "/fantasy-cricket/index.html",
+  "/fantasy-cricket/css/style.css",
+  "/fantasy-cricket/js/config.js",
+  "/fantasy-cricket/js/matches.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
